e8e9c6dccffe642ccaa8011f1642dc001f44617e
Philip Chung <philipchung1995@yahoo.com>
2018-12-14 00:35:56 -0800

    Add preliminary code for rendering random items

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,12 +1,27 @@
 <template>
+	<div>
+		<template v-for="item in items">
+			<set-item :item="item.itemName" :set="item.setName"/>
+		</template>
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import SetItem from './components/set-item.vue';
+import { getItems, SETS, ResultItem } from 'dwarves-days';
 
 @Component({
 	components: {
+		SetItem,
 	},
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+	items: ResultItem[];
+
+	constructor() {
+		super();
+		this.items = getItems(SETS);
+	}
+}
 </script>
---
+++ site/src/components/set-item.vue
@@ -0,0 +1,13 @@
+<template>
+	<p>{{ item }} ({{ set }})</p>
+</template>
+
+<script lang="ts">
+	import { Vue, Component, Prop } from 'vue-property-decorator';
+
+	@Component
+	export default class SetItem extends Vue {
+		@Prop(String) item!: string;
+		@Prop(String) set!: string;
+	}
+</script>
