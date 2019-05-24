055f76273cfe34c932d1871a838942a920efe991
Philip Chung <philipchung1995@yahoo.com>
2019-05-23 21:11:16 -0700

    Merge set item components and make it functional (and fix indentation)

--- site/src/components/set-items.vue
+++ site/src/components/set-items.vue
@@ -1,25 +1,22 @@
-<template>
+<template functional>
 	<v-list>
-		<set-item
-			v-for="item in items"
-			:key="item.itemName"
-			:item="item.itemName"
-			:explanation="item.setName"
-		/>
+		<v-list-group append-icon="" v-for="item in props.items" :key="itemName">
+			<v-list-tile slot="activator">
+				{{ item.itemName }}
+			</v-list-tile>
+			<v-list-tile color="grey darken-2">
+				{{ item.setName }}
+			</v-list-tile>
+		</v-list-group>
 	</v-list>
 </template>
 
 <script lang="ts">
-	import { Vue, Component, Prop } from 'vue-property-decorator';
-	import SetItem from './set-item.vue';
-	import { ResultItem } from 'dwarves-days';
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { ResultItem } from 'dwarves-days';
 
-	@Component({
-		components: {
-			SetItem,
-		}
-	})
-	export default class SetItems extends Vue {
-		@Prop(Array) items!: ResultItem[];
-	}
+@Component
+export default class SetItems extends Vue {
+	@Prop(Array) items!: ResultItem[];
+}
 </script>
