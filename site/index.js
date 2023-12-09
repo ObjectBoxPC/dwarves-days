53e7badca931150942dadb569a4001e8d80b576f
Philip Chung <philipchung1995@yahoo.com>
2023-12-08 21:53:32 -0800

    Remove RequireJS from site
    Require JS is no longer needed now that Dwarves Days 1.3.0 provides
    a single-file UMD module that supports a global.
    
    Note that a shim is required to alias the React and ReactDOM globals
    (used by React Bootstrap) to the preactCompat global.

--- site/index.js
+++ site/index.js
@@ -1,179 +1,152 @@
 'use strict';
-(function (document, require) {
-	require.config({
-		packages: [
-			{
-				name: 'dwarves-days',
-				location: 'https://cdn.jsdelivr.net/npm/dwarves-days@1.2.1/umd',
-				main: 'index',
-			},
-		],
-		paths: {
-			'react-bootstrap': 'https://cdn.jsdelivr.net/npm/react-bootstrap@1.6.7/dist/react-bootstrap.min',
-			'preact': 'https://cdn.jsdelivr.net/npm/preact@10.19.2/dist/preact.umd',
-			'preact/compat': 'https://cdn.jsdelivr.net/npm/preact@10.19.2/compat/dist/compat.umd',
-			'preact/hooks': 'https://cdn.jsdelivr.net/npm/preact@10.19.2/hooks/dist/hooks.umd',
-		},
-		map: {
-			'*': {
-				'react': 'preact/compat',
-				'react-dom': 'preact/compat',
-			},
-		},
-	});
+(function (document, Preact, PreactHooks, ReactBootstrap, DwarvesDays) {
+	var h = Preact.h;
 
-	require(
-		['preact', 'preact/hooks', 'react-bootstrap', 'dwarves-days'],
-		function (Preact, PreactHooks, ReactBootstrap, DwarvesDays) {
-			var h = Preact.h;
+	function App() {
+		var itemsState = PreactHooks.useState([]);
 
-			function App() {
-				var itemsState = PreactHooks.useState([]);
-
-				function updateItems() {
-					var items = DwarvesDays.getItems(DwarvesDays.SETS);
-					itemsState[1](items);
-				}
-
-				PreactHooks.useEffect(function () {
-					updateItems();
-				}, []);
-
-				if (itemsState[0].length === 0) {
-					return null;
-				}
-
-				return h(
-					ReactBootstrap.Container,
-					{ fluid: 'sm' },
-					h(
-						Question,
-						{ setName: itemsState[0][0].setName }
-					),
-					h(
-						Answer,
-						{ items: itemsState[0] }
-					),
-					h(
-						NewSetButton,
-						{ onClick: updateItems }
-					)
-				);
-			}
-
-			function Question(props) {
-				return h(
-					ReactBootstrap.Row,
-					null,
-					h(
-						ReactBootstrap.Col,
-						null,
-						h(
-							ReactBootstrap.Card,
-							{ className: 'question' },
-							h(
-								ReactBootstrap.Card.Body,
-								null,
-								h(
-									'p',
-									null,
-									'Can you name the ' + props.setName + '?'
-								)
-							)
-						)
-					)
-				);
-			}
-
-			function Answer(props) {
-				return h(
-					ReactBootstrap.Row,
-					null,
-					h(
-						ReactBootstrap.Col,
-						null,
-						h(
-							ReactBootstrap.Card,
-							{ className: 'answer' },
-							h(
-								ReactBootstrap.Card.Header,
-								null,
-								h(
-									'p',
-									null,
-									'Sure, there’s, um…'
-								)
-							),
-							h(
-								ReactBootstrap.Card.Body,
-								null,
-								h(
-									ReactBootstrap.Accordion,
-									{ as: 'ul' },
-									props.items.map(function (item, i) {
-										return h(
-											AnswerItem,
-											{
-												item: item,
-												index: i,
-												key: item.itemName,
-											}
-										);
-									})
-								)
-							)
-						)
-					)
-				);
-			}
-
-			function AnswerItem(props) {
-				return h(
-					ReactBootstrap.Card,
-					{ as: 'li' },
-					h(
-						ReactBootstrap.Accordion.Toggle,
-						{
-							as: ReactBootstrap.Button,
-							eventKey: String(props.index),
-							variant: 'light',
-							className: 'card-header text-left',
-						},
-						props.item.itemName
-					),
-					h(
-						ReactBootstrap.Accordion.Collapse,
-						{ eventKey: String(props.index) },
-						h(
-							ReactBootstrap.Card.Body,
-							null,
-							h(
-								'p',
-								null,
-								props.item.setName
-							)
-						)
-					)
-				);
-			}
-
-			function NewSetButton(props) {
-				return h(
-					ReactBootstrap.Row,
-					null,
-					h(
-						ReactBootstrap.Col,
-						null,
-						h(
-							ReactBootstrap.Button,
-							{ onClick: props.onClick, block: true },
-							'New Set'
-						)
-					)
-				);
-			}
-
-			Preact.render(h(App), document.getElementById('app'));
+		function updateItems() {
+			var items = DwarvesDays.getItems(DwarvesDays.SETS);
+			itemsState[1](items);
 		}
-	);
-})(document, require);
+
+		PreactHooks.useEffect(function () {
+			updateItems();
+		}, []);
+
+		if (itemsState[0].length === 0) {
+			return null;
+		}
+
+		return h(
+			ReactBootstrap.Container,
+			{ fluid: 'sm' },
+			h(
+				Question,
+				{ setName: itemsState[0][0].setName }
+			),
+			h(
+				Answer,
+				{ items: itemsState[0] }
+			),
+			h(
+				NewSetButton,
+				{ onClick: updateItems }
+			)
+		);
+	}
+
+	function Question(props) {
+		return h(
+			ReactBootstrap.Row,
+			null,
+			h(
+				ReactBootstrap.Col,
+				null,
+				h(
+					ReactBootstrap.Card,
+					{ className: 'question' },
+					h(
+						ReactBootstrap.Card.Body,
+						null,
+						h(
+							'p',
+							null,
+							'Can you name the ' + props.setName + '?'
+						)
+					)
+				)
+			)
+		);
+	}
+
+	function Answer(props) {
+		return h(
+			ReactBootstrap.Row,
+			null,
+			h(
+				ReactBootstrap.Col,
+				null,
+				h(
+					ReactBootstrap.Card,
+					{ className: 'answer' },
+					h(
+						ReactBootstrap.Card.Header,
+						null,
+						h(
+							'p',
+							null,
+							'Sure, there’s, um…'
+						)
+					),
+					h(
+						ReactBootstrap.Card.Body,
+						null,
+						h(
+							ReactBootstrap.Accordion,
+							{ as: 'ul' },
+							props.items.map(function (item, i) {
+								return h(
+									AnswerItem,
+									{
+										item: item,
+										index: i,
+										key: item.itemName,
+									}
+								);
+							})
+						)
+					)
+				)
+			)
+		);
+	}
+
+	function AnswerItem(props) {
+		return h(
+			ReactBootstrap.Card,
+			{ as: 'li' },
+			h(
+				ReactBootstrap.Accordion.Toggle,
+				{
+					as: ReactBootstrap.Button,
+					eventKey: String(props.index),
+					variant: 'light',
+					className: 'card-header text-left',
+				},
+				props.item.itemName
+			),
+			h(
+				ReactBootstrap.Accordion.Collapse,
+				{ eventKey: String(props.index) },
+				h(
+					ReactBootstrap.Card.Body,
+					null,
+					h(
+						'p',
+						null,
+						props.item.setName
+					)
+				)
+			)
+		);
+	}
+
+	function NewSetButton(props) {
+		return h(
+			ReactBootstrap.Row,
+			null,
+			h(
+				ReactBootstrap.Col,
+				null,
+				h(
+					ReactBootstrap.Button,
+					{ onClick: props.onClick, block: true },
+					'New Set'
+				)
+			)
+		);
+	}
+
+	Preact.render(h(App), document.getElementById('app'));
+})(document, preact, preactHooks, ReactBootstrap, dwarvesDays);
---
+++ site/preact-compat-shim.js
@@ -0,0 +1,1 @@
+React = ReactDOM = preactCompat;
