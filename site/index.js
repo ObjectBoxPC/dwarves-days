fef9554974224390bdb70334ba1a970aa0c47e63
Philip Chung <philipchung1995@yahoo.com>
2026-01-30 19:03:59 -0700

    Change to Simple.css for demo site

--- site/index.js
+++ site/index.js
@@ -1,5 +1,5 @@
 'use strict';
-(function (document, Preact, PreactHooks, ReactBootstrap, DwarvesDays) {
+(function (document, Preact, PreactHooks, DwarvesDays) {
 	var h = Preact.h;
 
 	function App() {
@@ -18,9 +18,7 @@
 			return null;
 		}
 
-		return h(
-			ReactBootstrap.Container,
-			{ fluid: 'sm' },
+		return [
 			h(
 				Question,
 				{ setName: itemsState[0][0].setName }
@@ -32,121 +30,53 @@
 			h(
 				NewSetButton,
 				{ onClick: updateItems }
-			)
-		);
+			),
+		];
 	}
 
 	function Question(props) {
 		return h(
-			ReactBootstrap.Row,
-			null,
-			h(
-				ReactBootstrap.Col,
-				null,
-				h(
-					ReactBootstrap.Card,
-					{ className: 'question' },
-					h(
-						ReactBootstrap.Card.Body,
-						null,
-						h(
-							'p',
-							null,
-							'Can you name the ' + props.setName + '?'
-						)
-					)
-				)
-			)
+			'p',
+			{ className: 'notice question' },
+			'Can you name the ' + props.setName + '?'
 		);
 	}
 
 	function Answer(props) {
 		return h(
-			ReactBootstrap.Row,
-			null,
+			'div',
+			{ className: 'notice answer' },
 			h(
-				ReactBootstrap.Col,
+				'p',
 				null,
-				h(
-					ReactBootstrap.Card,
-					{ className: 'answer' },
-					h(
-						ReactBootstrap.Card.Header,
-						null,
-						h(
-							'p',
-							null,
-							'Sure, there’s, um…'
-						)
-					),
-					h(
-						ReactBootstrap.Card.Body,
-						null,
-						h(
-							ReactBootstrap.Accordion,
-							{ as: 'ul' },
-							props.items.map(function (item, i) {
-								return h(
-									AnswerItem,
-									{
-										item: item,
-										index: i,
-										key: item.itemName,
-									}
-								);
-							})
-						)
-					)
-				)
-			)
-		);
-	}
-
-	function AnswerItem(props) {
-		return h(
-			ReactBootstrap.Card,
-			{ as: 'li' },
-			h(
-				ReactBootstrap.Accordion.Toggle,
-				{
-					as: ReactBootstrap.Button,
-					eventKey: String(props.index),
-					variant: 'light',
-					className: 'card-header text-left',
-				},
-				props.item.itemName
+				'Sure, there’s, um…'
 			),
-			h(
-				ReactBootstrap.Accordion.Collapse,
-				{ eventKey: String(props.index) },
-				h(
-					ReactBootstrap.Card.Body,
-					null,
+			props.items.map(function (item, i) {
+				return h(
+					'details',
+					{ key: item.itemName },
+					h(
+						'summary',
+						null,
+						item.itemName
+					),
 					h(
 						'p',
 						null,
-						props.item.setName
+						item.setName
 					)
-				)
-			)
+				);
+			})
 		);
 	}
 
 	function NewSetButton(props) {
 		return h(
-			ReactBootstrap.Row,
-			null,
-			h(
-				ReactBootstrap.Col,
-				null,
-				h(
-					ReactBootstrap.Button,
-					{ onClick: props.onClick, block: true },
-					'New Set'
-				)
-			)
+			'button',
+			{ onClick: props.onClick },
+			'New Set'
 		);
 	}
 
 	Preact.render(h(App), document.getElementById('app'));
-})(document, preact, preactHooks, ReactBootstrap, dwarvesDays);
+})(document, preact, preactHooks, dwarvesDays);
